8dc481e2bb61ab19b748b07f83f946144c80db29
Yohei Shinoduka <recurrent.shinoduka@gmail.com>
2021-06-14 10:22:02 +0900

    図書エンティティ

---
+++ workspace/spring-library/src/main/java/Bean/Books.java
@@ -0,0 +1,98 @@
+package Bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Books {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "books_id")
+	private int booksId;
+
+	@Column(name = "books_name")
+	private String booksName;
+
+	@Column(name = "books_author")
+	private String booksAuthor;
+
+	@Column(name = "books_stock")
+	private int booksStock;
+
+	@Column(name = "books_insert_date")
+	private String booksInsertDate;
+
+	@Column(name = "books_lend")
+	private String booksLend;
+
+	@Column(name = "books_remark")
+	private String booksRemark;
+
+	Books(){
+
+	}
+
+	public int getBooksId() {
+		return booksId;
+	}
+
+	public void setBooksId(int booksId) {
+		this.booksId = booksId;
+	}
+
+	public String getBooksName() {
+		return booksName;
+	}
+
+	public void setBooksName(String booksName) {
+		this.booksName = booksName;
+	}
+
+	public String getBooksAuthor() {
+		return booksAuthor;
+	}
+
+	public void setBooksAuthor(String booksAuthor) {
+		this.booksAuthor = booksAuthor;
+	}
+
+	public int getBooksStock() {
+		return booksStock;
+	}
+
+	public void setBooksStock(int booksStock) {
+		this.booksStock = booksStock;
+	}
+
+	public String getBooksInsertDate() {
+		return booksInsertDate;
+	}
+
+	public void setBooksInsertDate(String booksInsertDate) {
+		this.booksInsertDate = booksInsertDate;
+	}
+
+	public String getBooksLend() {
+		return booksLend;
+	}
+
+	public void setBooksLend(String booksLend) {
+		this.booksLend = booksLend;
+	}
+
+	public String getBooksRemark() {
+		return booksRemark;
+	}
+
+	public void setBooksRemark(String booksRemark) {
+		this.booksRemark = booksRemark;
+	}
+
+
+
+}
