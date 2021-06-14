df7da740331714e89c12e99f561c04ed90a7bd47
Yohei Shinoduka <recurrent.shinoduka@gmail.com>
2021-06-14 17:51:45 +0900

    図書エンティティ

--- workspace/spring-library/src/main/java/com/example/demo/Books.java
+++ workspace/spring-library/src/main/java/com/example/demo/Books.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -26,14 +26,14 @@ public class Books {
 	@Column(name = "books_stock")
 	private int booksStock;
 
-	@Column(name = "books_insert_date")
-	private Date booksInsertDate;
+	@Column(name = "books_registration")
+	private Date booksRegistration;
 
 	@Column(name = "books_lend")
 	private String booksLend;
 
-	@Column(name = "books_remark")
-	private String booksRemark;
+	@Column(name = "books_remarks")
+	private String booksRemarks;
 
 	@Column(name = "insert_date")
 	private Date insertDate;
@@ -50,110 +50,90 @@ public class Books {
 	@Column(name = "classification_id")
 	private int classificationId;
 
+
+
 	public int getBooksId() {
 		return booksId;
 	}
-
 	public void setBooksId(int booksId) {
 		this.booksId = booksId;
 	}
-
 	public String getBooksName() {
 		return booksName;
 	}
-
 	public void setBooksName(String booksName) {
 		this.booksName = booksName;
 	}
-
 	public String getBooksAuthor() {
 		return booksAuthor;
 	}
-
 	public void setBooksAuthor(String booksAuthor) {
 		this.booksAuthor = booksAuthor;
 	}
-
 	public int getBooksStock() {
 		return booksStock;
 	}
-
 	public void setBooksStock(int booksStock) {
 		this.booksStock = booksStock;
 	}
-
-	public Date getBooksInsertDate() {
-		return booksInsertDate;
+	public Date getBooksRegistration() {
+		return booksRegistration;
 	}
-
-	public void setBooksInsertDate(Date booksInsertDate) {
-		this.booksInsertDate = booksInsertDate;
+	public void setBooksRegistration(Date booksRegistration) {
+		this.booksRegistration = booksRegistration;
 	}
-
 	public String getBooksLend() {
 		return booksLend;
 	}
-
 	public void setBooksLend(String booksLend) {
 		this.booksLend = booksLend;
 	}
-
-	public String getBooksRemark() {
-		return booksRemark;
+	public String getBooksRemarks() {
+		return booksRemarks;
 	}
-
-	public void setBooksRemark(String booksRemark) {
-		this.booksRemark = booksRemark;
+	public void setBooksRemarks(String booksRemarks) {
+		this.booksRemarks = booksRemarks;
 	}
-
 	public Date getInsertDate() {
 		return insertDate;
 	}
-
 	public void setInsertDate(Date insertDate) {
 		this.insertDate = insertDate;
 	}
-
 	public int getInsertEmployeeId() {
 		return insertEmployeeId;
 	}
-
 	public void setInsertEmployeeId(int insertEmployeeId) {
 		this.insertEmployeeId = insertEmployeeId;
 	}
-
 	public Date getUpdateDate() {
 		return updateDate;
 	}
-
 	public void setUpdateDate(Date updateDate) {
 		this.updateDate = updateDate;
 	}
-
 	public int getUpdateEmployeeId() {
 		return updateEmployeeId;
 	}
-
 	public void setUpdateEmployeeId(int updateEmployeeId) {
 		this.updateEmployeeId = updateEmployeeId;
 	}
-
 	public int getClassificationId() {
 		return classificationId;
 	}
-
 	public void setClassificationId(int classificationId) {
 		this.classificationId = classificationId;
 	}
-
 	Books(){
 
 	}
-	Books(String booksName, String booksAuthor, int booksStock, String booksRemark, int classificationId){
+	Books(String booksName, String booksAuthor, int booksStock,Date booksRegistration, String booksLend,String booksRemarks, int classificationId){
 		this.booksName=booksName;
 		this.booksAuthor=booksAuthor;
 		this.booksStock=booksStock;
-		this.booksRemark=booksRemark;
+		this.booksRegistration=booksRegistration;
+		this.booksLend=booksLend;
+		this.booksRemarks=booksRemarks;
 		this.classificationId=classificationId;
 
 	}
