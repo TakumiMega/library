0a9077fd65b2216b2d9ac11ad49cbc644df9d44c
Yohei Shinoduka <recurrent.shinoduka@gmail.com>
2021-06-15 18:01:33 +0900

    著者検索完成させる

--- workspace/spring-library/src/main/java/com/example/demo/BooksController.java
+++ workspace/spring-library/src/main/java/com/example/demo/BooksController.java
@@ -14,17 +14,31 @@ import DAO.DAOException;
 @Controller
 public class BooksController {
 	@RequestMapping("/books")
-	public String loginPage() {
+	public String booksinfo() {
 		return "books";
 	}
 	@RequestMapping(value = "/library/serchBooks")
-	public ModelAndView showBooks(
+	public ModelAndView showBooksName(
 			@RequestParam("booksName") String booksName,
 			ModelAndView mv) throws DAOException{
 
 		BooksDAO dao = new BooksDAO();
-		List<BooksBean> booksList = dao.searchBooks(booksName);
+		List<BooksBean> booksList = dao.searchBooksName(booksName);
 		mv.addObject("booksName", booksName);
+		mv.addObject("booksList", booksList);
+
+		//表示させるHTMLをセット
+		mv.setViewName("books");
+
+		return mv;
+	}
+	public ModelAndView showBooksAuthor(
+			@RequestParam("booksAuthor") String booksAuthor,
+			ModelAndView mv) throws DAOException{
+
+		BooksDAO dao = new BooksDAO();
+		List<BooksBean> booksList = dao.searchBooksAuthor(booksAuthor);
+		mv.addObject("booksAuthor", booksAuthor);
 		mv.addObject("booksList", booksList);
 
 		//表示させるHTMLをセット
