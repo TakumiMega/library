48dbc891509686e2e2458ce62d305293b6b20aa7
Takumi Mega <recurrent.mega@gmail.com>
2021-06-16 09:25:50 +0900

    UsersRepository

--- workspace/spring-library/src/main/java/com/example/demo/UsersRepository.java
+++ workspace/spring-library/src/main/java/com/example/demo/UsersRepository.java
@@ -4,11 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
 public interface UsersRepository extends JpaRepository<Users, Integer>{
 	Users findByUsersId(Integer users_id);
-=======
-public interface UsersRepository extends JpaRepository <Users, Integer> {
 
->>>>>>> branch 'main' of git@github.com:TakumiMega/library.git
 }
