8b92b4377f4c453551875b01c2f9df92d15bf583
Shotaro Harada <recurrent.harada@gmail.com>
2021-06-16 10:26:28 +0900

    原田

--- workspace/spring-library/src/main/java/com/example/demo/UsersRepository.java
+++ workspace/spring-library/src/main/java/com/example/demo/UsersRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository <Users, Integer> {
+public interface UsersRepository extends JpaRepository<Users, Integer>{
 	Users findByUsersId(Integer users_id);
 
 	Users findByUsersNameAndUsersBirthday(String usersName, Date usersBirthday);
